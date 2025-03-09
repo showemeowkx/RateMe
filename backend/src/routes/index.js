6976410e2520db6d2c8b159e3f54ebe5efeab2b5
showemeowkx <sashacherepov55@gmail.com>
2025-03-09 13:29:07 +0100

    feat: add page for signing in - created 'acc-manage-crtl.js' controller - created 'acc-manage-rt.js' router - created a simple HTML document as a sign in page - set GET and POST methods for '/account' route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const mainRouter = require("./main-page-rt");
+const accManageRouter = require("./acc-manage-rt");
 
 const router = express.Router();
 
 router.use("/", mainRouter);
+router.use("/account", accManageRouter);
 
 module.exports = router;
