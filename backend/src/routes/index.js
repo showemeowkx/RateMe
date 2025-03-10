1a64b762968e8501b7b401ed7465b1da7260c004
showemeowkx <sashacherepov55@gmail.com>
2025-03-10 11:20:49 +0100

    feat: add page for review adding - created 'add-review-crtl.js' controller - created 'add-review-rt.js' router - created a simple HTML document as a page for adding reviews - set GET and POST methods for '/add-review' route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const mainRouter = require("./main-page-rt");
 const accManageRouter = require("./acc-manage-rt");
+const addReviewRouter = require("./add-review-rt");
 
 const router = express.Router();
 
 router.use("/", mainRouter);
 router.use("/account", accManageRouter);
+router.use("/add-review", addReviewRouter);
 
 module.exports = router;
