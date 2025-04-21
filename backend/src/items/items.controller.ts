107338ac4ebcb3b2ffe7e751fe1639c551a6a34a
showemeowkx <sashacherepov55@gmail.com>
2025-04-21 23:35:37 +0200

    feat (items): add a method for adding items

--- backend/src/items/items.controller.ts
+++ backend/src/items/items.controller.ts
@@ -1,8 +1,15 @@
-import { Controller, Logger } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { ItemsService } from './items.service';
+import { AddItemDto } from './dto/add-item.dto';
 
 @Controller('items')
 export class ItemsController {
   private logger = new Logger('ItemsController');
   constructor(private itemsService: ItemsService) {}
+
+  @Post('/add')
+  addItem(@Body() addItemDto: AddItemDto): Promise<void> {
+    this.logger.verbose(`Adding an item... {name: ${addItemDto.name}}`);
+    return this.itemsService.addItem(addItemDto);
+  }
 }
--- backend/src/items/items.service.ts
+++ backend/src/items/items.service.ts
@@ -1,12 +1,50 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Item } from './item.entity';
+import { AddItemDto } from './dto/add-item.dto';
 
 @Injectable()
 export class ItemsService {
-  private logger = new Logger('ItemsService');
+  private logger = new Logger('ItemsService', { timestamp: true });
   constructor(
     @InjectRepository(Item) private itemsRepository: Repository<Item>,
   ) {}
+
+  async addItem(addItemDto: AddItemDto): Promise<void> {
+    const { image, category, name, description } = addItemDto;
+
+    const item = this.itemsRepository.create({
+      image,
+      category,
+      name,
+      description,
+      rating: 0,
+      link: 'NO_LINK_PROVIDED',
+      reviews: '',
+    });
+
+    try {
+      await this.itemsRepository.save(item);
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      if (error.code === '23505') {
+        this.logger.error(
+          `[ALREADY EXISTS] Failed to add an item {name: ${name}}`,
+        );
+        throw new ConflictException('An item with this name already exists');
+      }
+      this.logger.error(
+        `[INTERNAL] Failed to add an item {name: ${name}}`,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        error.stack,
+      );
+      throw new InternalServerErrorException();
+    }
+  }
 }
