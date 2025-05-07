e6b4eb77b3d6437f897ce3d030174d220470d2a5
showemeowkx <sashacherepov55@gmail.com>
2025-05-07 17:42:52 +0200

    refactor (file-upload): add uploads directory initialization - changed the path to uploads directory - created a method that creates a directory if it doesn't exist - updated .gitignores

--- backend/src/common/file-upload.ts
+++ backend/src/common/file-upload.ts
@@ -1,16 +1,30 @@
 import { BadRequestException } from '@nestjs/common';
 import { diskStorage } from 'multer';
 import { v4 as uuidv4 } from 'uuid';
+import * as fs from 'fs';
 import * as path from 'path';
 
 export const setStorageOptions = (dir: string, allowedExtensions: string[]) => {
   return {
     storage: diskStorage({
-      destination: `./src/uploads/${dir}`,
+      destination: initDir(dir),
       filename: editFileName,
     }),
     fileFilter: validateFileType(allowedExtensions),
     limits: { fileSize: 5 * 1024 * 1024 },
+  };
+};
+
+const initDir = (dir: string) => {
+  return (
+    req,
+    file: Express.Multer.File,
+    cb: (error: Error | null, filename: string) => void,
+  ) => {
+    const uploadPath: string = path.join('uploads', dir);
+    fs.mkdirSync(uploadPath, { recursive: true });
+
+    cb(null, uploadPath);
   };
 };
 
