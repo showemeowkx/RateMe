18213741d5a280afa52b85b25a96a91768e3849b
showemeowkx <sashacherepov55@gmail.com>
2025-05-09 08:07:52 +0200

    feat (items): add more categories to the enum

--- backend/src/items/categories.enum.ts
+++ backend/src/items/categories.enum.ts
@@ -1,4 +1,11 @@
 export enum Categories {
   PHONES = 'phones',
-  OTHERS = 'others',
+  LAPTOPS = 'laptops',
+  TABLETS = 'tablets',
+  SMARTWATCHES = 'smartwatches',
+  MICE = 'mice',
+  KEYBOARDS = 'keyboards',
+  HEADPHONES = 'headphones',
+  ACCESSORIES = 'accessories',
+  OTHER = 'other',
 }
