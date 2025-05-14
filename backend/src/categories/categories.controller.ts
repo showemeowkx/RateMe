8a048a053f2fb09f77efccbfe98f4f28e577246c
showemeowkx <sashacherepov55@gmail.com>
2025-05-14 23:34:35 +0200

    feat (categories): implement logic for getting and creating categories - added  createCategory method which creates a category - added getCategories method which gets categories with filters - added getCategoriesBySlug method which gets a category by a slug - deleted a useless DTO file

--- backend/src/categories/categories.controller.ts
+++ backend/src/categories/categories.controller.ts
@@ -1,8 +1,57 @@
-import { Controller, Logger } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Logger,
+  Param,
+  Post,
+  Query,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { setStorageOptions } from 'src/common/file-upload';
+import { Category } from './category.entity';
 
+const allowedExtensions: string[] = ['.jpg', '.jpeg', '.png'];
+
+//will be guarded by moderator guard later
+@UseGuards(AuthGuard())
 @Controller('categories')
 export class CategoriesController {
   private logger = new Logger('CategoriesController');
   constructor(private categoriesService: CategoriesService) {}
+
+  @Get('/:slug')
+  getCategoryBySlug(@Param('slug') slug: string): Promise<Category> {
+    this.logger.verbose(`Getting a category by slug... {slug: ${slug}}`);
+    return this.categoriesService.getCategoryBySlug(slug);
+  }
+
+  @Get()
+  getCategories(@Query() name: string): Promise<Category[]> {
+    this.logger.verbose(`Getting categories... {filters: {name: ${name}}}`);
+    return this.categoriesService.getCategories(name);
+  }
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor(
+      'file',
+      setStorageOptions('category-images', allowedExtensions),
+    ),
+  )
+  createCategory(
+    @Body() createCategoryDto: CreateCategoryDto,
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<void> {
+    this.logger.verbose(
+      `Creating a new category... {slug: ${createCategoryDto.slug}}`,
+    );
+    return this.categoriesService.createCategory(createCategoryDto, file);
+  }
 }
--- backend/src/categories/categories.service.ts
+++ backend/src/categories/categories.service.ts
@@ -1,7 +1,16 @@
-import { Injectable, Logger } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from './category.entity';
 import { Repository } from 'typeorm';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import * as fs from 'fs/promises';
 
 @Injectable()
 export class CategoriesService {
@@ -10,4 +19,65 @@ export class CategoriesService {
     @InjectRepository(Category)
     private categoriesRepository: Repository<Category>,
   ) {}
+
+  async getCategories(name: string): Promise<Category[]> {
+    const query = this.categoriesRepository.createQueryBuilder('category');
+
+    if (name) {
+      query.andWhere('category.name LIKE :name', { name: `%${name}%` });
+    }
+    try {
+      return query.getMany();
+    } catch (error) {
+      this.logger.error(
+        `[INTERNAL] Failed to get categories {filters: {name: ${name}}}`,
+        error.stack,
+      );
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async getCategoryBySlug(slug: string): Promise<Category> {
+    const category = await this.categoriesRepository.findOneBy({ slug });
+
+    if (!category) {
+      this.logger.error(`[NOT FOUND] Failed to get a category {slug: ${slug}}`);
+      throw new NotFoundException(`Category with slug ${slug} was not found`);
+    }
+
+    return category;
+  }
+
+  async createCategory(
+    createCategoryDto: CreateCategoryDto,
+    file: Express.Multer.File,
+  ): Promise<void> {
+    const { name, slug, color } = createCategoryDto;
+    const imagePath = file.path;
+
+    const category = this.categoriesRepository.create({
+      name,
+      slug,
+      color,
+      imagePath,
+    });
+
+    try {
+      await this.categoriesRepository.save(category);
+    } catch (error) {
+      if (file?.path) await fs.unlink(file.path);
+      if (error.code === '23505') {
+        this.logger.error(
+          `[ALREADY EXISTS] Failed to create a category {slug: ${slug}}`,
+          error.stack,
+        );
+        throw new ConflictException('A category with this slug already exists');
+      }
+      this.logger.error(
+        `[INTERNAL] Failed to create a category {slug: ${slug}}`,
+        error.stack,
+      );
+      throw new InternalServerErrorException();
+    }
+  }
 }
