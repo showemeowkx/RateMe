57c6e0e2c697ce863fb6d86f23f22bac36330555
showemeowkx <sashacherepov55@gmail.com>
2025-05-18 23:25:14 +0200

    refactor: create a directory for decorators and update imports

--- backend/src/decorators/get-user.decorator.ts
+++ backend/src/decorators/get-user.decorator.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from './user.entity';
+import { User } from 'src/auth/user.entity';
 
 export const GetUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): User => {
