9eccc2e96ad96506f57abffda2c07c6c0de51f60
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-05-18 23:46:11 +0300

    feat(services): add async function to fetch products from API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,15 @@
-export default async function fetchCategories() {
+export const fetchCategories = async () => {
   const response = await fetch('http://localhost:3001/categories');
   if (!response.ok) {
-    throw new Error('Failed to fetch');
+    throw new Error('Failed to fetch categories');
   }
   return response.json();
-}
+};
+
+export const fetchProducts = async () => {
+  const response = await fetch('http://localhost:3001/items');
+  if (!response.ok) {
+    throw new Error('Failed to fetch products');
+  }
+  return response.json();
+};
