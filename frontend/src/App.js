399b094d86668e97c5ecd0d2d47bd0ec245a1904
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-05-18 23:59:17 +0300

    feat(App): configure routing to products page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/products' element={<Products />} />
         </Routes>
       </Router>
     </div>
