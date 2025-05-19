de06bf3ca6bab3f15764c2daa8c5e9857bc827ec
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-05-19 18:52:38 +0300

    feat(Products): add sorting for products using memoization

--- frontend/src/components/ProductsPage/ProductsGrid.jsx
+++ frontend/src/components/ProductsPage/ProductsGrid.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import { useState, useMemo } from 'react';
 import ProductsCard from './ProductsCard';
 import styles from './ProductsGrid.module.css';
 
 export default function ProductsGrid({ products }) {
+  const [sorting, setSorting] = useState('★ Найкращі');
+
+  const handleSortingChange = (e) => {
+    setSorting(e.target.value);
+  };
+
+  const matchesSorting = (sort) => {
+    if (sort === '★ Найкращі') return (a, b) => b.rating - a.rating;
+    if (sort === '☆ Найгірші') return (a, b) => a.rating - b.rating;
+    return () => 0;
+  };
+
+  const sortedProducts = useMemo(() => {
+    const sortingFn = matchesSorting(sorting);
+    return [...products].sort(sortingFn);
+  }, [products, sorting]);
+
   return (
-    <div className={styles.productGrid}>
-      {products.map((product) => (
-        <ProductsCard product={product} key={product.id} />
-      ))}
+    <div>
+      <div>
+        <label>Сортування</label>
+        <select value={sorting} onChange={handleSortingChange}>
+          <option value='★ Найкращі'>★ Найкращі</option>
+          <option value='☆ Найгірші'>☆ Найгірші</option>
+        </select>
+      </div>
+      <div className={styles.productGrid}>
+        {sortedProducts.map((product) => (
+          <ProductsCard product={product} key={product.id} />
+        ))}
+      </div>
     </div>
   );
 }
