74a0da483bf385f5595a201e5719f53f125d53c0
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-05-21 15:24:45 +0300

    feat(Products): add sorting and searching logic

--- frontend/src/components/ProductsPage/ProductsGrid.jsx
+++ frontend/src/components/ProductsPage/ProductsGrid.jsx
@@ -1,43 +1,72 @@
 import { useState, useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
 import ProductsCard from './ProductsCard';
 import styles from './ProductsGrid.module.css';
 
+export const ProductSortKind = {
+  BEST: '★ Найкращі',
+  WORST: '☆ Найгірші',
+};
+
+const useQuery = () => new URLSearchParams(useLocation().search);
+
 export default function ProductsGrid({ products }) {
-  const [sorting, setSorting] = useState('★ Найкращі');
+  const [sorting, setSorting] = useState(ProductSortKind.BEST);
+  const query = useQuery().get('search')?.toLowerCase() || '';
 
   const handleSortingChange = (e) => {
     setSorting(e.target.value);
   };
 
   const matchesSorting = (sort) => {
-    if (sort === '★ Найкращі') return (a, b) => b.rating - a.rating;
-    if (sort === '☆ Найгірші') return (a, b) => a.rating - b.rating;
+    if (sort === ProductSortKind.BEST) return (a, b) => b.rating - a.rating;
+    if (sort === ProductSortKind.WORST) return (a, b) => a.rating - b.rating;
     return () => 0;
   };
 
+  const filteredProducts = useMemo(() => {
+    return products.filter((product) =>
+      product.name.toLowerCase().includes(query)
+    );
+  }, [products, query]);
+
   const sortedProducts = useMemo(() => {
     const sortingFn = matchesSorting(sorting);
-    return [...products].sort(sortingFn);
-  }, [products, sorting]);
+    return [...filteredProducts].sort(sortingFn);
+  }, [filteredProducts, sorting]);
 
   return (
-    <div>
-      <div className={styles.sorting}>
-        <label>Сортування</label>
-        <select
-          className={styles.dropdown}
-          value={sorting}
-          onChange={handleSortingChange}
-        >
-          <option value='★ Найкращі'>★ Найкращі</option>
-          <option value='☆ Найгірші'>☆ Найгірші</option>
-        </select>
-      </div>
-      <div className={styles.productGrid}>
-        {sortedProducts.map((product) => (
-          <ProductsCard product={product} key={product.id} />
-        ))}
-      </div>
-    </div>
+    <>
+      {sortedProducts.length ? (
+        <div>
+          <div className={styles.sorting}>
+            <label>Сортування</label>
+            <select
+              className={styles.dropdown}
+              value={sorting}
+              onChange={handleSortingChange}
+            >
+              <option value={ProductSortKind.BEST}>
+                {ProductSortKind.BEST}
+              </option>
+              <option value={ProductSortKind.WORST}>
+                {ProductSortKind.WORST}
+              </option>
+            </select>
+          </div>
+          <div className={styles.productGrid}>
+            {sortedProducts.map((product) => (
+              <ProductsCard product={product} key={product.id} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <img
+          src='/assets/not_searchable.jpg'
+          alt='not_searchable'
+          className={styles.defaultImg}
+        />
+      )}
+    </>
   );
 }
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
 import styles from './SearchBar.module.css';
 import { FaSearch } from 'react-icons/fa';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function SearchBar() {
+  const [searchContent, setSearchContent] = useState('');
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleSearchingChange = (e) => {
+    setSearchContent(e.target.value);
+  };
+
+  const clearSearch = () => {
+    setSearchContent('');
+  };
+
+  const handleEnterDown = (e) => {
+    if (e.key === 'Enter' && searchContent.trim()) {
+      clearSearch();
+      const encoded = encodeURIComponent(searchContent.trim());
+      location.pathname === '/products'
+        ? navigate(`/products?search=${encoded}`, { replace: true })
+        : navigate(`/products?search=${encoded}`);
+    }
+  };
+
   return (
     <div className={styles.searchContainer}>
       <FaSearch className={styles.searchIcon} />
@@ -10,6 +33,9 @@ export default function SearchBar() {
         type='text'
         className={styles.searchInput}
         placeholder='Пошук...'
+        value={searchContent}
+        onChange={handleSearchingChange}
+        onKeyDown={handleEnterDown}
       />
     </div>
   );
