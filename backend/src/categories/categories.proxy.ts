af36316bf0e6624e7b2b741f502fd88dfba884d6
showemeowkx <sashacherepov55@gmail.com>
2025-05-26 19:36:56 +0200

    feat (categories): add cache to the proxy getter

--- backend/src/categories/categories.proxy.ts
+++ backend/src/categories/categories.proxy.ts
@@ -4,18 +4,30 @@ import { Category } from './category.entity';
 import { GetCategoriesFilterDto } from './dto/get-categories-filter.dto';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { CategoriesServiceIInterface } from './categories-service.interface';
+import * as NodeCache from 'node-cache';
 
 @Injectable()
 export class CategoriesProxy implements CategoriesServiceIInterface {
   private logger = new Logger('CategoriesProxy');
+  private cache = new NodeCache({ stdTTL: 600 });
 
   constructor(private categoriesService: CategoriesService) {}
 
   async getCategories(filterDto: GetCategoriesFilterDto): Promise<Category[]> {
+    const cacheKey = JSON.stringify(filterDto);
+    const cachedCategories = this.cache.get<Category[]>(cacheKey);
+    if (cachedCategories) {
+      this.logger.verbose(
+        `[CACHED] Getting categories... {filters: ${JSON.stringify(filterDto)}}`,
+      );
+      return cachedCategories;
+    }
     this.logger.verbose(
       `Getting categories... {filters: ${JSON.stringify(filterDto)}}`,
     );
-    return this.categoriesService.getCategories(filterDto);
+    const categories = await this.categoriesService.getCategories(filterDto);
+    this.cache.set(cacheKey, categories);
+    return categories;
   }
 
   async createCategory(
