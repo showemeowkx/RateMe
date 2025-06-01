b0cb3e35e4497e1c80b861fab1474d7d5fba2bef
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-06-01 23:16:13 +0300

    fix: update sign up fetching function

--- frontend/src/services/auth/signUp.js
+++ frontend/src/services/auth/signUp.js
@@ -2,9 +2,19 @@ import { URL, fetchData } from '../api';
 
 export const signUp = async (dto) => {
   try {
+    const params = new URLSearchParams();
+    params.append('name', dto.name);
+    params.append('surname', dto.surname);
+    params.append('username', dto.username);
+    params.append('email', dto.email);
+    params.append('password', dto.password);
+
     await fetchData(`${URL}/auth/signup`, {
       method: 'POST',
-      body: JSON.stringify(dto),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: params.toString(),
     });
   } catch (err) {
     throw new Error(err.message);
