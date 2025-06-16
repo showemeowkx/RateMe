1f712531d194dbe1471c45815488aebc2591161d
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-06-16 21:50:59 +0300

    fix: update signUp function using fetchData function

--- frontend/src/services/auth/signUp.js
+++ frontend/src/services/auth/signUp.js
@@ -1,5 +1,6 @@
 import { URL } from '../api';
 import { signIn } from './signIn';
+import { fetchData } from '../api';
 
 export const signUp = async (dto) => {
   const params = new URLSearchParams();
@@ -9,21 +10,13 @@ export const signUp = async (dto) => {
   params.append('email', dto.email);
   params.append('password', dto.password);
 
-  const response = await fetch(`${URL}/auth/signup`, {
+  await fetchData(`${URL}/auth/signup`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
     body: params.toString(),
   });
-
-  if (!response.ok) {
-    const errorData = await response.json();
-    const error = new Error('Validation error');
-    error.status = response.status;
-    error.messages = errorData.message;
-    throw error;
-  }
 
   await signIn({ login: dto.username, password: dto.password });
 };
