a781fabde37d2e2ed2fd884bdde40a5394956b09
showemeowkx <sashacherepov55@gmail.com>
2025-12-20 21:47:05 +0100

    feat (categories): update category creation logic with new Cloudinary service

--- backend/src/categories/categories.controller.ts
+++ backend/src/categories/categories.controller.ts
@@ -13,13 +13,10 @@ import {
 } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { setStorageOptions } from 'src/common/file-upload';
 import { Category } from './category.entity';
 import { GetCategoriesFilterDto } from './dto/get-categories-filter.dto';
 import { ModeratorGuard } from 'src/common/decorators/guards/moderator.guard';
 import { CategoriesServiceIInterface } from './categories-service.interface';
-
-const allowedExtensions: string[] = ['.jpg', '.jpeg', '.png'];
 
 @Controller('categories')
 export class CategoriesController {
@@ -37,12 +34,7 @@ export class CategoriesController {
 
   @Post()
   @UseGuards(ModeratorGuard)
-  @UseInterceptors(
-    FileInterceptor(
-      'file',
-      setStorageOptions('category-images', allowedExtensions),
-    ),
-  )
+  @UseInterceptors(FileInterceptor('file'))
   createCategory(
     @Body() createCategoryDto: CreateCategoryDto,
     @UploadedFile() file: Express.Multer.File,
